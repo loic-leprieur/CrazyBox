6e49902f12ebb70523bc59aeb4db7ce47b93b63d
Bertrand Muller <bertrand.muller3@etu.univ-lorraine.fr>
2016-02-10 22:33:39 +0100

    Modification de VueAccueil

--- src/views/VueAccueil.php
+++ src/views/VueAccueil.php
@@ -15,8 +15,7 @@ class VueAccueil{
     }
 
     public function nonConnecte(){
-        $html=<<<END
-<!DOCTYPE html>
+        $html = '<!DOCTYPE html>
 <html lang="fr">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
@@ -141,7 +140,7 @@ class VueAccueil{
       <div class="row">
         <div class="col l6 s12">
           <h5 class="white-text">Company Bio</h5>
-          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+          <p class="grey-text text-lighten-4">We are a team of college students working on this project like its our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
 
 
         </div>
@@ -179,8 +178,8 @@ class VueAccueil{
   <script src="js/init.js"></script>
 
   </body>
-</html>
-END;
+</html>';
+      
         echo $html;
     }
 }
