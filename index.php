90d426d708ecd5bc59ba3fa7f1939f2691019f9b
loic <donleprieur@outlook.fr>
2016-02-10 22:17:22 +0100

    - Création de l'index - Route vers la page Accueil - Vue de l'accueil - Controleur principale (dont fonction d'accès à l'accueil)

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: loic
+ * Date: 10/02/16
+ * Time: 21:32
+ */
+
+include 'vendor/autoload.php';
+
+// démarrer une session
+// etablir la connexion avec la BD
+
+$app = new \Slim\Slim();
+
+$app->get('/accueil', function(){
+$controller = new controller\GeneralController();
+$controller->accueil();
+});
