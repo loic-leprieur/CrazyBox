ecdc92356210cad2d1779a76e5aed6f6730814cf
Maxime Cailac <cailacmaxime@gmail.com>
2016-02-11 22:52:23 +0100

    Visualisation pas à pas

--- js/cagnotte.js
+++ js/cagnotte.js
@@ -1,8 +1,15 @@
 var tab = document.getElementById("tabVisu");
 var i = 1;
 tab.onclick = function () {
-	console.log(this.childNodes[3]);
-	this.childNodes[3].childNodes[i].style.display="none";
-	i+=2;
-	this.childNodes[3].childNodes[i].style.display="table-row";
+	if(i == ((this.childNodes.length)-1)) {
+		var p = document.createElement("p");
+		var text = document.createTextNode("Fin de la box !");
+		p.appendChild(text);
+		this.childNodes[3].childNodes[i].insertBefore(p, this.childNodes[3].childNodes[i].firstChild);
+	}
+	else {
+		this.childNodes[3].childNodes[i].style.display="none";
+		i+=2;
+		this.childNodes[3].childNodes[i].style.display="table-row";
+	}
 };
