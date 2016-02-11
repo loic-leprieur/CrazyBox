8d2235ea99173af0668a968bc5eb111ae15adeaf
Florian LEDOUX <florian.ledoux1@etu.univ-lorraine.fr>
2016-02-11 20:52:28 +0100

    Mise a jour du crazyDB.sql

--- temporaire/CrazyDB.sql
+++ temporaire/CrazyDB.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 11, 2016 at 08:47
+-- Generation Time: Feb 11, 2016 at 08:51
 -- Server version: 10.0.17-MariaDB
 -- PHP Version: 5.6.14
 
@@ -23,6 +23,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cagnotte`
+--
+
+CREATE TABLE `cagnotte` (
+  `idPochette` int(11) NOT NULL,
+  `montantActuel` double(7,2) NOT NULL DEFAULT '0.00',
+  `atteinte` char(1) NOT NULL,
+  `id_url` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contient`
+--
+
+CREATE TABLE `contient` (
+  `idPochette` int(11) NOT NULL,
+  `idPrestation` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pochette`
 --
 
@@ -35,14 +59,109 @@ CREATE TABLE `pochette` (
   `privee` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prestation`
+--
+
+CREATE TABLE `prestation` (
+  `id` int(11) NOT NULL,
+  `nom` text NOT NULL,
+  `descr` text NOT NULL,
+  `type` int(11) NOT NULL,
+  `img` text NOT NULL,
+  `prix` decimal(5,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `prestation`
+--
+
+INSERT INTO `prestation` (`id`, `nom`, `descr`, `type`, `img`, `prix`) VALUES
+(1, 'Champagne', 'Bouteille de champagne + flutes + jeux à gratter', 1, 'champagne.jpg', '20.00'),
+(2, 'Musique', 'Partitions de piano à 4 mains', 1, 'musique.jpg', '25.00'),
+(3, 'Exposition', 'Visite guidée de l’exposition ‘REGARDER’ à la galerie Poirel', 2, 'poirelregarder.jpg', '14.00'),
+(4, 'Goûter', 'Goûter au FIFNL', 3, 'gouter.jpg', '20.00'),
+(5, 'Projection', 'Projection courts-métrages au FIFNL', 2, 'film.jpg', '10.00'),
+(6, 'Bouquet', 'Bouquet de roses et Mots de Marion Renaud', 1, 'rose.jpg', '16.00'),
+(7, 'Diner Stanislas', 'Diner à La Table du Bon Roi Stanislas (Apéritif /Entrée / Plat / Vin / Dessert / Café / Digestif)', 3, 'bonroi.jpg', '60.00'),
+(8, 'Origami', 'Baguettes magiques en Origami en buvant un thé', 3, 'origami.jpg', '12.00'),
+(9, 'Livres', 'Livre bricolage avec petits-enfants + Roman', 1, 'bricolage.jpg', '24.00'),
+(10, 'Diner  Grand Rue ', 'Diner au Grand’Ru(e) (Apéritif / Entrée / Plat / Vin / Dessert / Café)', 3, 'grandrue.jpg', '59.00'),
+(11, 'Visite guidée', 'Visite guidée personnalisée de Saint-Epvre jusqu’à Stanislas', 2, 'place.jpg', '11.00'),
+(12, 'Bijoux', 'Bijoux de manteau + Sous-verre pochette de disque + Lait après-soleil', 1, 'bijoux.jpg', '29.00'),
+(13, 'Opéra', 'Concert commenté à l’Opéra', 2, 'opera.jpg', '15.00'),
+(14, 'Thé Hotel de la reine', 'Thé de debriefing au bar de l’Hotel de la reine', 3, 'hotelreine.gif', '5.00'),
+(15, 'Jeu connaissance', 'Jeu pour faire connaissance', 2, 'connaissance.jpg', '6.00'),
+(16, 'Diner', 'Diner (Apéritif / Plat / Vin / Dessert / Café)', 3, 'diner.jpg', '40.00'),
+(17, 'Cadeaux individuels', 'Cadeaux individuels sur le thème de la soirée', 1, 'cadeaux.jpg', '13.00'),
+(18, 'Animation', 'Activité animée par un intervenant extérieur', 2, 'animateur.jpg', '9.00'),
+(19, 'Jeu contacts', 'Jeu pour échange de contacts', 2, 'contact.png', '5.00'),
+(20, 'Cocktail', 'Cocktail de fin de soirée', 3, 'cocktail.jpg', '12.00'),
+(21, 'Star Wars', 'Star Wars - Le Réveil de la Force. Séance cinéma 3D', 2, 'starwars.jpg', '12.00'),
+(22, 'Concert', 'Un concert à Nancy', 2, 'concert.jpg', '17.00'),
+(23, 'Appart Hotel', 'Appart’hôtel Coeur de Ville, en plein centre-ville', 4, 'apparthotel.jpg', '56.00'),
+(24, 'Hôtel d''Haussonville', 'Hôtel d''Haussonville, au coeur de la Vieille ville à deux pas de la place Stanislas', 4, 'hotel_haussonville_logo.jpg', '169.00'),
+(25, 'Boite de nuit', 'Discothèque, Boîte tendance avec des soirées à thème & DJ invités', 2, 'boitedenuit.jpg', '32.00'),
+(26, 'Planètes Laser', 'Laser game : Gilet électronique et pistolet laser comme matériel, vous voilà équipé.', 2, 'laser.jpg', '15.00'),
+(27, 'Fort Aventure', 'Découvrez Fort Aventure à Bainville-sur-Madon, un site Accropierre unique en Lorraine ! Des Parcours Acrobatiques pour petits et grands, Jeu Mission Aventure, Crypte de Crapahute, Tyrolienne, Saut à l''élastique inversé, Toboggan géant... et bien plus encore.', 2, 'fort.jpg', '25.00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type`
+--
+
+CREATE TABLE `type` (
+  `id` int(11) NOT NULL,
+  `nom` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `type`
+--
+
+INSERT INTO `type` (`id`, `nom`) VALUES
+(1, 'Attention'),
+(2, 'Activité'),
+(3, 'Restauration'),
+(4, 'Hébergement');
+
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `cagnotte`
+--
+ALTER TABLE `cagnotte`
+  ADD PRIMARY KEY (`idPochette`);
+
+--
+-- Indexes for table `contient`
+--
+ALTER TABLE `contient`
+  ADD PRIMARY KEY (`idPochette`,`idPrestation`),
+  ADD KEY `fk_prestation` (`idPrestation`);
+
+--
 -- Indexes for table `pochette`
 --
 ALTER TABLE `pochette`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `prestation`
+--
+ALTER TABLE `prestation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `type` (`type`);
+
+--
+-- Indexes for table `type`
+--
+ALTER TABLE `type`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -54,6 +173,39 @@ ALTER TABLE `pochette`
 --
 ALTER TABLE `pochette`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `prestation`
+--
+ALTER TABLE `prestation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+--
+-- AUTO_INCREMENT for table `type`
+--
+ALTER TABLE `type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cagnotte`
+--
+ALTER TABLE `cagnotte`
+  ADD CONSTRAINT `fk_idPochette` FOREIGN KEY (`idPochette`) REFERENCES `pochette` (`id`);
+
+--
+-- Constraints for table `contient`
+--
+ALTER TABLE `contient`
+  ADD CONSTRAINT `fk_idPochette2` FOREIGN KEY (`idPochette`) REFERENCES `cagnotte` (`idPochette`),
+  ADD CONSTRAINT `fk_prestation` FOREIGN KEY (`idPrestation`) REFERENCES `prestation` (`id`);
+
+--
+-- Constraints for table `prestation`
+--
+ALTER TABLE `prestation`
+  ADD CONSTRAINT `prestation_ibfk_1` FOREIGN KEY (`type`) REFERENCES `type` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
