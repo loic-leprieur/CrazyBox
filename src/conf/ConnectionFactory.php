10372d52b64a8aac88ae7e97b114ed9569ac2a33
Maxime Cailac <cailacmaxime@gmail.com>
2016-02-11 14:00:52 +0100

    Ajout d'une connectin factory et premiers tests d'affichage de la liste dans le catalogue

---
+++ src/conf/ConnectionFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace conf;
+
+use Illuminate\Database\Capsule\Manager as DB;
+
+/**
+ * Classe qui permet créer une connection à une base de données avec des paramètres
+ */
+class ConnectionFactory {
+
+	/**
+	 * Le tableau qui contiendra les paramètres
+	 */
+	private static $config = array();
+	/**
+	 * La connection
+	 */
+	private static $connection = null;
+
+	/**
+	 * Fonction qui permet de modifier les paramètres de la connection
+	 */
+	public static function setConfig($fichier) {
+		// fichier de config pour la connection
+		self::$config = parse_ini_file($fichier);
+	}
+
+	/**
+	 * Fonction statique qui permet de récupérer une connection à une base de données
+	 */
+	public static function makeConnection() {
+		if (self::$connection == null) {
+			// creation de la connection avec eloquent
+			self::$connection = new DB();
+			self::$connection->addConnection(self::$config);
+			self::$connection->setAsGlobal();
+			self::$connection->bootEloquent();
+		}
+		return self::$connection;	
+	}
+
+}
