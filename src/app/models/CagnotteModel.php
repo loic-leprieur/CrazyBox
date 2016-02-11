1f580f5b0df4acd5ccc38a9db27a5d822b47688b
Maxime Cailac <cailacmaxime@gmail.com>
2016-02-11 12:04:37 +0100

    Ajout des modèles pour les 3 nouvelles tables : cagnotte, contient et pochette

---
+++ src/app/models/CagnotteModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class CagnotteModel qui représente le modèle de la table "cagnotte" de la base de données
+ * @package app\models
+ */
+class CagnotteModel extends Model {
+
+    /**
+     * Table cagnotte de la base de données
+     * @var string Nom de la table cagnotte
+     */
+    protected $table = 'cagnotte';
+
+    /**
+     * Clé primaire dans la table cagnotte
+     * @var string Clé primaire de la table cagnotte
+     */
+    protected $primaryKey = 'idPochette';
+
+    public $timestamps = false;
+}
